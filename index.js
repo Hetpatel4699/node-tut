24c9cd38db0999982123cf5dc5ce7acc45b77ce7
Hetpatel4699 <hetpatel4699@gmail.com>
2023-09-15 11:05:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// console.log("het")
+
+// let a=20;
+// var b=20;
+// const c=30;
+// c=200
+// console.warn(a+b+c);
+
+
+// // == example
+// var a='20';
+// if(a===20)
+// {
+//     console.log("matched")
+// }
+
+// // === example
+// var a='20';
+// if(a===20)
+// {
+//     console.log("matched")
+// }
+
+
+// const app = require('./app')  
+// // console.log(app.z())
+
+// // filter method
+// const arr =[4,7,2,5,8,3];
+//  let result =arr.filter((item)=>{
+//     return item>2
+//     // console.log(item);
+// })
+// console.warn(result)
+
+
+// const fs=require('fs');
+// console.log("-->",__dirname)
+// // fs.writeFileSync("hello2.txt","How are you every budy!!")
+// // console.log("code step by step");
+
+
+// const http = require('http');
+
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>Hello this is abc</h1>");
+//     resp.end();
+// }).listen(4000);
+
+// const http = require('http')
+// const dataControl = (req,resp) =>
+// {
+//     resp.write("<h1>Hello How Are You</h1>");
+//     resp.end();
+// }
+
+// http.createServer(dataControl).listen(31000)
+
+// const http = require('http');
+// function test(a)
+// {
+//     return a*10;
+// }
+// // const test = (a)=>a*100
+// http.createServer((req,resp)=>{
+//     resp.write("<h1>Hello this is abc</h1>");
+//     resp.end();
+// }).listen(31000);
+
+
+
+console.log("package.json")
