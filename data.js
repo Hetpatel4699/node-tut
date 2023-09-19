a045c0a3d25776a033f4a30f718a39f0e3cb7a1e
Hetpatel4699 <hetpatel4699@gmail.com>
2023-09-19 13:26:00 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,6 @@
+const data =[
+    {name:'Het Patel', email:'het1@test.com',contact:'9998'},
+    {name:'Harsh Patel',email:'harsh@test.com'},
+    {name:'Preet Patel',email:'preet@test.com'}
+    ]
+module.exports=data;
--- index.js
+++ index.js
@@ -69,4 +69,38 @@
 
 
 
-console.log("package.json")
+// const color = require('colors');
+// console.log("Het".black)
+
+// const http = require('http');
+// const data = require('./data')
+// http.createServer((req,resp)=>{
+//   resp.writeHead(200,{'content-type':'application\json'});
+//   resp.write(JSON.stringify(data));
+//   resp.end();
+// }).listen(4200)
+
+// const fs = require('fs');
+// const input = process.argv;
+
+// if(input[2]=='add')
+// {
+//     fs.writeFileSync(input[3],input[4])
+// }else if(input[2]=='remove')
+// {
+//     fs.unlinkSync(input[3])
+// }
+// else{
+//     console.log("invalid input")
+// }
+
+
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname,'files');   
+// console.log(dirPath)
+for(i=0;i<5;i++)
+{
+    fs.writeFileSync(dirPath+"/hello"+i+".txt","A simple text file")
+    // fs.writeFileSync(`hello${i}.txt`,"A simple text file")
+}
